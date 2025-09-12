86b98a31edeb8aeb938d5c3bf7628b0b5e6a1e98
Hector Uribe <dgic.h_uribe@gobjuarez.mpio>
2025-09-12 13:17:35 -0600

    Se habilita ip de servidor

--- parques_jardines/settings.py
+++ parques_jardines/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = '_61-pJgQMEV6rPL7JI_O758wTIh3SqCjNwTMGrr1fSvfBLI9fV8xjqkOGO59suQyel
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.236.62.93']
 
 # Application definition
 INSTALLED_APPS = [
